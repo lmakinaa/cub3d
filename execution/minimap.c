6e7533dbd3518f566a46ca0b756e3f9887549802
Mohamed Amine Iguiji <miguiji@e1r1p1.1337.ma>
2024-05-24 02:26:39 +0100

    hmm

--- execution/minimap.c
+++ execution/minimap.c
@@ -6,7 +6,7 @@
 /*   By: miguiji <miguiji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 19:05:18 by ijaija            #+#    #+#             */
-/*   Updated: 2024/05/24 01:07:54 by miguiji          ###   ########.fr       */
+/*   Updated: 2024/05/24 02:26:30 by miguiji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,45 @@ void	draw_tile(t_cub *cub, int x, int y, int color)
 	}
 }
 
-void	draw_mini_player(t_cub *cub, int x, int y, int color)
-{
-	int	i;
-	int	j;
-
-	i = -1;
-	while (++i < MINI_TILE_SIZE / 2)
-	{
-		j = -1;
-		while (++j < MINI_TILE_SIZE / 2)
-			mlx_put_pixel(cub->minimap_img, x + i, y + j, color);
-	}
-	x += MINI_TILE_SIZE / 4;
-	y += MINI_TILE_SIZE / 4;
-	double ang = -M_PI/6;
-	while(ang <= M_PI/6)
-	{
-	draw_line(cub, x, y, x + cos(cub->p->angle + ang) * 50,y + sin(cub->p->angle + ang) * 50, color);
-	ang += 0.01;
-	}
+int major(double a) {
+    return (int)ceil(a);
 }
 
+void	draw_mini_player(t_cub *cub, int x, int y, int color)
+{
+    int	i;
+    int	j;
+    int	var_x;
+    int	var_y;
+    i = -1;
+    while (++i < MINI_TILE_SIZE / 2)
+    {
+        j = -1;
+        while (++j < MINI_TILE_SIZE / 2)
+            mlx_put_pixel(cub->minimap_img, x + i, y + j, color);
+    }
+    x += MINI_TILE_SIZE / 4;
+    y += MINI_TILE_SIZE / 4;
+    double ang = -M_PI/6;
+    while(ang <= M_PI/6)
+    {
+        double ray_x = 0;
+        double ray_y = 0;
+        while(1)
+        {
+            ray_x += cos(cub->p->angle + ang) * 0.1;
+            ray_y += sin(cub->p->angle + ang) * 0.1;
+            var_x = (x + ray_x) / TILE_SIZE;
+            var_y = (y + ray_y) / TILE_SIZE;
+            if (cub->data->map2d[var_y][var_x] != '1')
+                continue;
+            else
+                break;
+        }
+        draw_line(cub, x, y, x + ray_x, y + ray_y, color);
+        ang += 0.01;
+    }
+}
 void	draw_minimap(t_cub *cub)
 {
 	int		x;
