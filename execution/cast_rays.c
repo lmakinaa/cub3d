e58f7bc71a3727bc20ae49291de3f835d1ccee0c
Ismail Jaija <ijaija@e2r6p4.1337.ma>
2024-05-24 17:12:00 +0100

    hmm

--- execution/cast_rays.c
+++ execution/cast_rays.c
@@ -6,7 +6,7 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 19:17:40 by ijaija            #+#    #+#             */
-/*   Updated: 2024/05/24 17:04:00 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/05/24 17:08:07 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,26 @@ void cast_rays(t_cub *cub, int x, int y)
 	int	col;
 
 	cub->ray->ray_angle = cub->p->angle - (cub->p->fov_rd / 2);
-	for (int i = 0; i < cub->ray->n_rays; i++)
+	col = -1;
+	while (++col < cub->ray->n_rays)
 	{
+		double ray_x = 0;
+        double ray_y = 0;
+		while (1)
+		{
+			ray_x += cos(cub->ray->ray_angle) * 0.1;
+            ray_y += sin(cub->ray->ray_angle) * 0.1;
+            int var_x = (x + ray_x) / TILE_SIZE;
+            int var_y = (y + ray_y) / TILE_SIZE;
+			if (cub->data->map2d[var_y][var_x] != '1')
+                continue;
+            else
+                break;
+		}
 		draw_line(cub, x, y,
-		x + cos(cub->ray->ray_angle) * 0.1,
-		y + sin(cub->ray->ray_angle) * 0.1,
+		x + ray_x,
+		y + ray_y,
 		get_rgba(0, 0, 255, 255));
-
 		cub->ray->ray_angle += (cub->p->fov_rd / cub->ray->n_rays);
-		col++;
 	}
 }
-
